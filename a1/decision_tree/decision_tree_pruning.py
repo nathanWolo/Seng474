0c3dce6246a1a668e8dbed866d04a91c5d6c8490
Nathan Woloshyn <nathanwoloshyn@gmail.com>
2023-02-02 16:06:51 -0800

    Getting started on random forests

--- a1/decision_tree/decision_tree_pruning.py
+++ a1/decision_tree/decision_tree_pruning.py
@@ -11,20 +11,48 @@ train, test = read_data.partition_data(read_data.read_data())
 #create dt
 dt = DecisionTreeClassifier(random_state=0, criterion="entropy").fit(train.drop(columns=['income']), train['income'])
 
-#test dt
-print(dt.score(test.drop(columns=['income']), test['income']))
-
 #prune dt
 path = dt.cost_complexity_pruning_path(train.drop(columns=['income']), train['income'])
 ccp_alphas, impurities = path.ccp_alphas, path.impurities
 
-trees = []
+depths = []
+scores = []
 i = 0
+best_alpha = ccp_alphas[0]
+best_score = 0
 for ccp_alpha in ccp_alphas:
+    if i % 100 == 0:
+        print(i)
+    dt_pruned = DecisionTreeClassifier(random_state=0, criterion="entropy", ccp_alpha=ccp_alpha).fit(train.drop(columns=['income']), train['income'])
+    if dt_pruned.score(test.drop(columns=['income']), test['income']) > best_score:
+            best_score = dt_pruned.score(test.drop(columns=['income']), test['income'])
+            best_alpha = ccp_alpha
+    depths.append(dt_pruned.get_depth())
+    scores.append(dt_pruned.score(test.drop(columns=['income']), test['income']))
     i+=1
-    print("Iteration: ", i)
-    dt = DecisionTreeClassifier(random_state=0, criterion="entropy", ccp_alpha=ccp_alpha)
-    dt.fit(train.drop(columns=['income']), train['income'])
-    trees.append(dt)
 
-print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(trees[-1].tree_.node_count, ccp_alphas[-1]))
+'''Plot the scores on both the training and test sets using matplotlib from varying alpha values'''
+
+def plot_alpha_scores():
+    plt.plot(ccp_alphas, scores, label='Scores')
+    plt.plot(best_alpha, best_score, 'ro', label='Best score: '
+        + "{:.4f}".format(best_score) + ' at alpha: ' + str(best_alpha))
+    plt.legend(loc="upper right")
+    plt.xlabel('Alpha')
+    plt.ylabel('Score')
+    plt.title('Entropy, pruning')
+    plt.savefig('entropy_pruning_scores_varying_alpha.png')
+    plt.clf()
+
+'''plot the depths as alpha varies'''
+def plot_alpha_depths():
+    plt.plot(ccp_alphas, depths, label='Depth')
+    plt.legend(loc="upper right")
+    plt.xlabel('Alpha')
+    plt.ylabel('Depth')
+    plt.title('Entropy, pruning')
+    plt.savefig('entropy_pruning_depths_varying_alpha.png')
+    plt.clf()
+
+plot_alpha_scores()
+plot_alpha_depths()
---
+++ a1/random_forest/random_forest.py
@@ -0,0 +1,99 @@
+from sklearn.ensemble import RandomForestClassifier
+import sys
+import numpy as np
+sys.path.append('..')
+from utils import read_data
+import matplotlib.pyplot as plt
+
+
+train, test = read_data.partition_data(read_data.read_data()) 
+num_features = len(train.columns) - 1
+num_estimators = int(np.floor(np.sqrt(num_features)))
+
+
+'''Test various depths, with entropy as the criterion, 
+store and plot the scores on both training and test sets using matplotlib'''
+def test_entropy_depths():
+    best_depth = 0
+    best_score = 0
+    test_scores = []
+    training_scores = []
+    for i in range(1, 30):
+        model = RandomForestClassifier(n_estimators=num_estimators,max_depth=i, random_state=0, criterion="entropy")
+        model.fit(train.drop(columns=['income']), train['income'])
+        test_scores.append(model.score(test.drop(columns=['income']), test['income']))
+        training_scores.append(model.score(train.drop(columns=['income']), train['income']))
+        if model.score(test.drop(columns=['income']), test['income']) > best_score:
+            best_score = model.score(test.drop(columns=['income']), test['income'])
+            best_depth = i
+    plt.plot(range(1, 30), test_scores, label='Test')
+    plt.plot(range(1, 30), training_scores, label='Training')
+    plt.plot(best_depth, best_score, 'ro', label='Best score: '
+        + "{:.4f}".format(best_score) + ' at depth: ' + str(best_depth))
+    plt.legend(loc="best")
+    plt.xlabel('Depth')
+    plt.ylabel('Score')
+    plt.title('Entropy, no pruning')
+    #plt.show()
+    plt.savefig('rf_entropy_scores_varying_depth.png')
+    plt.clf()
+
+
+'''Test various depths, with gini as the criterion'''
+
+def test_gini_depths():
+    best_depth = 0
+    best_score = 0
+    test_scores = []
+    training_scores = []
+    for i in range(1, 30):
+        model = RandomForestClassifier(n_estimators=num_estimators,max_depth=i, random_state=0, criterion="gini")
+        model.fit(train.drop(columns=['income']), train['income'])
+        test_scores.append(model.score(test.drop(columns=['income']), test['income']))
+        training_scores.append(model.score(train.drop(columns=['income']), train['income']))
+        if model.score(test.drop(columns=['income']), test['income']) > best_score:
+            best_score = model.score(test.drop(columns=['income']), test['income'])
+            best_depth = i
+    plt.plot(range(1, 30), test_scores, label='Test')
+    plt.plot(range(1, 30), training_scores, label='Training')
+    plt.plot(best_depth, best_score, 'ro', label='Best score: '
+        + "{:.4f}".format(best_score) + ' at depth: ' + str(best_depth))
+    plt.legend(loc="best")
+    plt.xlabel('Depth')
+    plt.ylabel('Score')
+    plt.title('Gini, no pruning')
+    #plt.show()
+    plt.savefig('rf_gini_scores_varying_depth.png')
+    plt.clf()
+
+# test_entropy_depths()
+# test_gini_depths()
+
+'''test various number of estimators, with gini as the criterion, depth 15'''
+
+def test_gini_estimators():
+    best_num_estimators = 0
+    best_score = 0
+    test_scores = []
+    training_scores = []
+    for i in range(1, 100):
+        model = RandomForestClassifier(n_estimators=i,max_depth=15, random_state=0, criterion="gini")
+        model.fit(train.drop(columns=['income']), train['income'])
+        test_scores.append(model.score(test.drop(columns=['income']), test['income']))
+        training_scores.append(model.score(train.drop(columns=['income']), train['income']))
+        if model.score(test.drop(columns=['income']), test['income']) > best_score:
+            best_score = model.score(test.drop(columns=['income']), test['income'])
+            best_num_estimators = i
+    plt.plot(range(1, 100), test_scores, label='Test')
+    plt.plot(range(1, 100), training_scores, label='Training')
+    plt.plot(best_num_estimators, best_score, 'ro', label='Best score: '
+        + "{:.4f}".format(best_score) + ' at num estimators: ' + str(best_num_estimators))
+    plt.legend(loc="best")
+    plt.xlabel('Number of estimators')
+    plt.ylabel('Score')
+    plt.title('Gini, depth 15')
+    #plt.show()
+    plt.savefig('rf_gini_scores_varying_estimators.png')
+    plt.clf()
+
+test_gini_estimators()
