ceba69631b462a2d9831f81411c1afe9f1c198a4
Nathan Woloshyn <nathanwoloshyn@gmail.com>
2023-02-26 16:48:41 -0800

    updated gamma & C search for gaussian kernel

--- a2/svm/gaussian_svm.py
+++ a2/svm/gaussian_svm.py
@@ -4,10 +4,10 @@ from utils.read_data import read_data, filter_data
 from k_fold_cross_validation.kfcv import k_fold_cross_validation, k_fold_separation
 import sklearn.svm as svm
 import matplotlib.pyplot as plt
-
+import time
 train_df, test_df = read_data()
 
-train_df = filter_data(train_df).sample(frac=0.5, random_state=1234)
+train_df = filter_data(train_df).sample(frac=1, random_state=1234)
 test_df = filter_data(test_df)
 
 folds = k_fold_separation(train_df)
@@ -22,8 +22,9 @@ def plot_error_gaussian_svm_varying_gamme_and_c(gamma_values):
     best_gamma = 0
     c_gammas = []
     for gamma in gamma_values:
+        start_time = time.perf_counter()
         print('gamma: ', gamma)
-        cur_gamma_c_values = [1.5**i for i in range(-3, 5)]
+        cur_gamma_c_values = [1.5**i for i in range(-5, 8)]
         best_c = 0
         best_c_score = 0
         for c in cur_gamma_c_values:
@@ -34,10 +35,12 @@ def plot_error_gaussian_svm_varying_gamme_and_c(gamma_values):
                 best_c_score = cur_c_score
                 best_c = c
         c_gammas.append(best_c)
+        print(best_c)
         gs = svm.SVC(C=best_c, gamma=gamma)
         gs.fit(train_df.iloc[:, :-1], train_df.iloc[:, -1])
         cur_gamma_score = gs.score(test_df.iloc[:, :-1], test_df.iloc[:, -1])
         accuracies.append(cur_gamma_score)
+        print(time.perf_counter() - start_time)
         if cur_gamma_score > best_acc:
             best_acc = cur_gamma_score
             best_gamma = gamma
@@ -52,6 +55,6 @@ def plot_error_gaussian_svm_varying_gamme_and_c(gamma_values):
     plt.legend(loc='best')
     plt.savefig('error_vs_gamma.png')
 
-gamma_values = [(1.5**i) for i in range(-2, 8)]
+gamma_values = [(1.5**i) for i in range(-3, 8)]
 plot_error_gaussian_svm_varying_gamme_and_c(gamma_values)
 
